21e5b92d7df61b986cd4a1c5920acd05e1d21954
Gina Rizka <ginarizkaa@gmail.com>
2019-08-27 09:56:10 +0700

    oke

--- common/models/DataUser.js
+++ common/models/DataUser.js
@@ -1,3 +1,40 @@
 module.exports = function(DataUser) {
-    
+    DataUser.remoteMethod(
+        'getDataBySpv', 
+        {
+            description: 'get data by Id Spv',
+            accepts:[
+                {arg: 'idSpv', type: 'string'}
+            ],
+            returns: {
+                arg: 'res', type: 'object', root: true
+            },
+            http: { path: '/getDataBySpv', verb: 'get'}
+        }
+    );
+    DataUser.getDataBySpv = function(idSpv, callback){
+        new Promise(function(resolve, reject){ //fungsi promise untuk menjalankan code sesuai dengan urutannya
+            
+            var filter = {
+                where: {
+                    idSpv : idSpv
+                }
+            }
+            DataUser.find(filter, function(err, result){
+                if(err) reject (err)
+                if (result === null){
+                    err = new Error('Cannot find that name')
+                    err.statusCode = 404
+                    reject (err)
+                }
+
+                resolve (result)
+            })
+        }).then(function(res){
+            if (!res) callback (err)
+            return callback (null, res)
+        }).catch(function(err){
+            callback(err);
+        });
+    }
 };
